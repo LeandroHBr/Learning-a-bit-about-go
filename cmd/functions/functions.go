eb2cbfb4469505f7122e3925b1c070f27ea701b4
LeandroHBr <leandrobraga242@gmail.com>
2024-09-06 13:42:28 -0300

    functions

---
+++ cmd/functions/functions.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func greet(name string, age int) {
+	fmt.Println("hi my name is", name, "and i have ", age, "year old")
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	/*
+		greet("Leandro", 18)
+		greet("hélio", 29)*/
+
+	var c int = sum(1, 2)
+	println(c)
+
+}
